4a6fa3c78e78bb8291677c7798f256ff629484aa
André Steffens <aner.steffens@hotmail.com>
2020-01-26 20:37:05 +0100

    Scrollviewer for the popup

--- WPF/Converters/BoolToStringConverter.cs
+++ WPF/Converters/BoolToStringConverter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BridgeTimer
+namespace BridgeTimer.Converters
 {
     public class BoolToStringConverter : BoolToValueConverter<string>
     {}
